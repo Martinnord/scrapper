aaf99287ca33e4a247b617f8051cd2a9e84c16db
martinnord <martin.nordstrom99@gmail.com>
2018-10-17 21:29:57 +0200

    Added recursive function to get passed pagination

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
+  const extractPartners = async url => {
+    const page = await browser.newPage();
+    await page.goto(url);
+    console.log(`Scrapping page: ${url}`);
+    const scrappedParners = await page.evaluate(() =>
+      Array.from(document.querySelectorAll("div.compact")).map(parnter => ({
+        title: parnter.querySelector("h3.title").innerText.trim(),
+        logo: parnter.querySelector(".logo img").src
+      }))
+    );
+
+    await page.close();
+
+    // Stop the recursion
+    if (scrappedParners.length === 0) {
+      console.log(`Ended recursion at url: ${url}`);
+      return scrappedParners;
+    } else {
+      const nextPageNumber = parseInt(url.match(/page=(\d+)$/)[1], 10) + 1;
+      const nextUrl = `https://marketingplatform.google.com/about/partners/find-a-partner?page=${nextPageNumber}`;
+      return scrappedParners.concat(await extractPartners(nextUrl));
+    }
+  };
+
   const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  const url =
-    "https://marketingplatform.google.com/about/partners/find-a-partner?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fpartners%2F";
-  await page.goto(url);
-  //await page.screenshot({ path: "example.png" });
+  const startUrl = "https://marketingplatform.google.com/about/partners/find-a-partner?page=44";
 
-  const partners = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("div.compact"))
-    .map(parnter => ({
-        title: parnter.querySelector('h3.title').innerText.trim(),
-        logo: parnter.querySelector('.logo img').src,
-    }))
-  );
-
+  const partners = await extractPartners(startUrl);
   console.log(partners);
 
   await browser.close();
