6b5aee63d9696501b8b39e56728c887f8cae4cee
Martin Nordström <martin.nordstrom99@gmail.com>
2018-10-17 21:01:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const url =
+    "https://marketingplatform.google.com/about/partners/find-a-partner?utm_source=marketingplatform.google.com&utm_medium=et&utm_campaign=marketingplatform.google.com%2Fabout%2Fpartners%2F";
+  await page.goto(url);
+  //await page.screenshot({ path: "example.png" });
+
+  const partners = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("div.compact"))
+    .map(parnter => ({
+        title: parnter.querySelector('h3.title').innerText.trim(),
+        logo: parnter.querySelector('.logo img').src,
+    }))
+  );
+
+  console.log(partners);
+
+  await browser.close();
+})();
